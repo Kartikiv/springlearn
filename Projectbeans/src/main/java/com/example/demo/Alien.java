5b35c6f8be79253681fc8418b4f954fd887dd9fa
karth <karth@kartik>
2023-01-16 10:23:03 +0530

    as sysout

--- Projectbeans/src/main/java/com/example/demo/Alien.java
+++ Projectbeans/src/main/java/com/example/demo/Alien.java
@@ -4,6 +4,7 @@ public class Alien implements LivingThings {
 	private int start;
   public Alien(int start) {
 	  this.start=start;
+	  System.out.println(start);
   }
 	@Override
 	public String start() {
